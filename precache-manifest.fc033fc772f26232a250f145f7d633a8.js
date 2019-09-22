1df8a1f8859e875235093b46950f5fba8d58ecf0
jayalakshmi.s <jayalakshmi.s@comviva.com>
2019-09-22 23:45:30 +0530

    Updates

---
+++ precache-manifest.fc033fc772f26232a250f145f7d633a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e90baaf7e0d4e6e854841ce32f01eb29",
+    "url": "/react-cart/index.html"
+  },
+  {
+    "revision": "9ad86ca7ac64f8ab2ebb",
+    "url": "/react-cart/static/css/main.1bbdeede.chunk.css"
+  },
+  {
+    "revision": "3394561a276a15cd6386",
+    "url": "/react-cart/static/js/2.1ddd281f.chunk.js"
+  },
+  {
+    "revision": "9ad86ca7ac64f8ab2ebb",
+    "url": "/react-cart/static/js/main.c5007488.chunk.js"
+  },
+  {
+    "revision": "5a65cb31ae868a01cf94",
+    "url": "/react-cart/static/js/runtime-main.c4190368.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/padmakant-baloji/react-cart/home/precache-manifest.b155358c2deaead3ea9c938ca0ae3620.js"
+  "/react-cart/precache-manifest.fc033fc772f26232a250f145f7d633a8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/padmakant-baloji/react-cart/home/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-cart/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
